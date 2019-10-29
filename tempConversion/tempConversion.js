5f94fffa01f2c77a8b6793df5f8e880203d953ee
Tobias Klöpper <tobiaskloepper1@web.de>
2019-10-29 19:39:42 +0100

    solves exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(isDegree)
+{
+    let newDegree = (isDegree - 32) * (5/9);
+    newDegree = Math.round(newDegree * 10) / 10;
+    return newDegree;
 }
 
-const ctof = function() {
-
+const ctof = function(isDegree) 
+{
+    let newDegree = (isDegree * (9/5)) + 32;
+    newDegree = Math.round(newDegree * 10) / 10;
+    return newDegree;
 }
 
 module.exports = {
