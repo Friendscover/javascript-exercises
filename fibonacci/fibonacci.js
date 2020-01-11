aec26586a51920469363b36f01d4b46d5df1a879
Friendscover <tobiaskloepper1@web.de>
2020-01-11 14:21:32 +0100

    Finished Fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberOfLoops) {
+    if(numberOfLoops > 0)
+    {
+        let fibonacciArray = [1, 1];
+        for (i= 1; i < numberOfLoops -1; i++)
+        { 
+            let firstFiboNumber = fibonacciArray[i];
+            let secondFiboNumber = fibonacciArray[i - 1];
+            let fiboToAdd = parseInt(firstFiboNumber + secondFiboNumber);
+            fibonacciArray.push(fiboToAdd);
+            //console.log(fibonacciArray);
+        }
+        return fibonacciArray.pop();
+    }
+    else 
+    {
+        return "OOPS";
+    }
 }
 
 module.exports = fibonacci
