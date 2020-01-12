83c130cb19c0d6900b5c9e510de2e4d18ccc8129
Friendscover <tobiaskloepper1@web.de>
2020-01-12 11:34:11 +0100

    Finished getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookArray = [];
+    for(i = 0; i < books.length; i++)
+    {
+        bookArray.push(books[i].title);
+    }  
+    return bookArray;
 }
 
 module.exports = getTheTitles;
